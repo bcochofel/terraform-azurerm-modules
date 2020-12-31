695ef28491459beb80ee2ccf3f1abe931b9f451d
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-12-31 12:45:43 +0000

    resource group module testing #minor

--- test/resource_group_example_test.go
+++ test/resource_group_example_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/azure"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
